3150de607d8b7c1a5da6c0c7189d9700a585e279
kazem <kazem>
2024-01-12 13:31:39 -0500

    fix: fix tail -f like reading from files

--- xapps/rnti_imsi.py
+++ xapps/rnti_imsi.py
@@ -5,26 +5,33 @@ import logging
 from configs import rnti_imsi_path, rnti_teid_path, imsi_teid_path, TEID_IMSI_UPDATE_PERIOD, RNTI_TEID_UPDATE_PERIOD
 logging.basicConfig(level=logging.INFO) # DEBUG INFO
 
+
+rnti_imsi_dict = {}
+teid_imsi_dict = {}
+rnti_teid_dict = {}
+
+wait = True
+
+
 def dump_mapping():
     logging.debug("dump_mapping is invoked")
-    global rnti_imsi_dict
     print('rnti_imsi_dict: ', rnti_imsi_dict)
     rnti_imsi_json = json.dumps(rnti_imsi_dict)
     with open(rnti_imsi_path, "w") as outfile:
         outfile.write(rnti_imsi_json)
     
+
 def build_rnti_imsi_mapping():
     logging.debug("build_rnti_imsi_mapping is invoked")
-    global rnti_imsi_dict
     for rnti, teid in rnti_teid_dict.items():
         while not teid in teid_imsi_dict:
             time.sleep(TEID_IMSI_UPDATE_PERIOD * 3)
             print("teid_imsi_dict not up to date")
         rnti_imsi_dict[rnti] = teid_imsi_dict[teid]
 
-####################
-#### RNTI TO TEID 
-####################
+    dump_mapping()
+
+
 class RntiTeid(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -52,44 +59,32 @@ class RntiTeid(threading.Thread):
         else:
             logging.warning("skipping the line: {}".format(line))
 
+    def read_file(self, file):
+        updated = False
+        while True:
+            line = file.readline()
+            if line is None or line == "":
+                return updated
+            if len(line.split(" ")) == 3:
+                updated = True
+                self.update_rnti_teid_dict(line)
     
     def run(self):
         # iterate through the existing lines
         rnti_teid = open(rnti_teid_path, "r")
-        for iter, line in enumerate(rnti_teid.readlines()):
-            if len(line.split(" ")) == 3:
-                self.update_rnti_teid_dict(line)
-            else:
-                pass
-            last_line_count = iter+1    
-        rnti_teid.close()
+        self.read_file(rnti_teid)
 
         # build the rnti_imsi mapping
         build_rnti_imsi_mapping()
-        dump_mapping()
 
         # poll new lines and process them
         while True:
-            rnti_teid = open(rnti_teid_path, "r")
-            lines = rnti_teid.readlines()
-            if len(lines) > last_line_count:
-                for i in range(last_line_count, len(lines)):
-                    last_line_count += 1
-                    if len(lines[i].split(" ")) == 3:
-                        self.update_rnti_teid_dict(lines[i])
-                    else:
-                        pass
-
+            updated = self.read_file(rnti_teid)
+            if updated:
                 build_rnti_imsi_mapping()
-                dump_mapping()
-
-            rnti_teid.close()
             time.sleep(RNTI_TEID_UPDATE_PERIOD)
 
 
-####################
-#### TEID TO IMSI
-####################
 class TeidImsi(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -100,18 +95,21 @@ class TeidImsi(threading.Thread):
             suffix = '\n'
             teid_imsi_dict[line_items[2][:-len(suffix)]] = line_items[1]
         else:
-            raise "file corrupted"
+            raise Exception("file corrupted")
+
+    def read_all_remaining_lines(self, file):
+        # iterate through the existing lines
+        while True:
+            line = file.readline()
+            if line is None or line == "":
+                return
+            if len(line.split(" ")) == 3:
+                self.update_teid_imsi_dict(line)
 
     def run(self):
         # iterate through the existing lines
-        teid_imsi = open(imsi_teid_path, "r")
-        for iter, line in enumerate(teid_imsi.readlines()):
-            if len(line.split(" ")) == 3:
-                self.update_teid_imsi_dict(line)
-            else:
-                pass
-            last_line_count = iter+1
-        teid_imsi.close()
+        teid_imsi_file = open(imsi_teid_path, "r")
+        self.read_all_remaining_lines(teid_imsi_file)
 
         # start processing the other file
         global wait
@@ -119,40 +117,15 @@ class TeidImsi(threading.Thread):
 
         # poll new lines and process them
         while True:
-            teid_imsi = open(imsi_teid_path, "r")
-            lines = teid_imsi.readlines()
-            if len(lines) > last_line_count:
-                for i in range(last_line_count, len(lines)):
-                    last_line_count += 1
-                    if len(lines[i].split(" ")) == 3:
-                        self.update_teid_imsi_dict(lines[i])
-                    else:
-                        pass
-
-            teid_imsi.close()
+            self.read_all_remaining_lines(teid_imsi_file)
             time.sleep(TEID_IMSI_UPDATE_PERIOD)
 
+
 if __name__ == '__main__':
-    # RNTI-IMSI
-    global rnti_imsi_dict
-    rnti_imsi_dict = dict()
-
-    ## TEID-IMSI
-    global teid_imsi_dict
-    teid_imsi_dict = dict()
-
-    teid_imsi_thread = TeidImsi()
-    teid_imsi_thread.start()
-    
-    global wait
-    wait = True
+    TeidImsi().start()
 
     while wait:
         time.sleep(0.1)
 
-    ## RNTI-TEID
-    global rnti_teid_dict
-    rnti_teid_dict = dict()
+    RntiTeid().start()
 
-    rnti_teid_thread = RntiTeid()
-    rnti_teid_thread.start()
